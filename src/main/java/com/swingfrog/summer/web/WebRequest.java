4b2ff1d9f1200a9478e152b2c6f196a3fbe9d6ef
SwingFrog <swingfrog@qq.com>
2021-02-23 11:13:08 +0800

    update

--- src/main/java/com/swingfrog/summer/web/WebRequest.java
+++ src/main/java/com/swingfrog/summer/web/WebRequest.java
@@ -23,11 +23,12 @@ public class WebRequest extends SessionRequest {
 		WebRequest webRequest = new WebRequest();
 		webRequest.setHttpRequest(httpRequest);
 		JSONObject data = new JSONObject();
-		if (uri.contains("?")) {
-			String[] texts = uri.split("\\?");
-			if (texts.length == 2) {
-				uri = texts[0];
-				String query = texts[1];
+		String currentUri = uri;
+		int index = currentUri.indexOf("?");
+		if (index > -1) {
+			currentUri = uri.substring(0, index);
+			if (index < uri.length()) {
+				String query = uri.substring(index + 1);
 				if (query.length() > 0) {
 					String[] value = query.split("&");
 					for (String keyValue : value) {
@@ -40,18 +41,18 @@ public class WebRequest extends SessionRequest {
 				}
 			}
 		}
-		webRequest.setPath(uri);
-		String remoteMethod = uri.substring(1);
+		webRequest.setPath(currentUri);
+		String remoteMethod = currentUri.substring(1);
 		if (RemoteDispatchMgr.get().containsRemoteMethod(remoteMethod)) {
 			webRequest.setDynamic(true);
 			webRequest.setRemote(remoteMethod);
 			webRequest.setMethod(null);
 		} else {
-			if (uri.contains(".")) {
+			if (currentUri.contains(".")) {
 				webRequest.setDynamic(false);
 			} else {
 				webRequest.setDynamic(true);
-				String[] remoteMethods = uri.split("_");
+				String[] remoteMethods = currentUri.split("_");
 				if (remoteMethods.length == 2) {
 					webRequest.setRemote(remoteMethods[0].substring(1));
 					webRequest.setMethod(remoteMethods[1]);
@@ -94,5 +95,5 @@ public class WebRequest extends SessionRequest {
 	public String toString() {
 		return "WebRequest [remote=" + getRemote() + ", method=" + getMethod() + ", data=" + getData() + "]";
 	}
-	
+
 }
